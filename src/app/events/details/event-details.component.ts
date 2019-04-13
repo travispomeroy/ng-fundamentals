687a1e17ddd171b992c40ee5fd7eb7292792cf92
Travis Pomeroy <travis.pomeroy84@gmail.com>
2019-04-13 17:18:23 -0400

    Pulled the event id from the url in the EventDetailsComponent.

--- src/app/events/details/event-details.component.ts
+++ src/app/events/details/event-details.component.ts
@@ -1,16 +1,17 @@
 import {Component, OnInit} from "@angular/core";
 import {EventService} from "../shared/event.service";
+import {ActivatedRoute} from "@angular/router";
 
 @Component(
   {
     templateUrl: './event-details.component.html',
     styles: [
-      `
+        `
         .container {
           padding-left: 20px;
           padding-right: 20px;
         }
-        
+
         .event-image {
           height: 100px;
         }
@@ -18,15 +19,16 @@ import {EventService} from "../shared/event.service";
     ]
   }
 )
-export class EventDetailsComponent implements OnInit{
+export class EventDetailsComponent implements OnInit {
 
-  private event:any;
+  private event: any;
 
-  constructor(private eventService:EventService) {
+  constructor(private eventService: EventService, private activatedRoute: ActivatedRoute) {
 
   }
 
   ngOnInit(): void {
-    this.event = this.eventService.getEvent(1);
+    let idParameter: number = this.activatedRoute.snapshot.params['id'];
+    this.event = this.eventService.getEvent(+idParameter);
   }
 }
