b92491beffbb2d9652e581b3735cc1ee1e1e6bf2
Travis Pomeroy <travis.pomeroy84@gmail.com>
2019-04-13 20:51:27 -0400

    Added a form group for updating the first and last name of the currently logged in user.

--- src/app/user/profile.component.ts
+++ src/app/user/profile.component.ts
@@ -1,18 +1,33 @@
-import { Component } from '@angular/core'
+import {Component, OnInit} from '@angular/core'
+import {FormControl, FormGroup} from "@angular/forms";
+import {AuthenticationService} from "./authentication/authentication.service";
+import {Router} from "@angular/router";
 
 @Component({
-  template: `
-    <h1>Edit Your Profile</h1>
-    <hr>
-    <div class="col-md-6">
-      <h3>[Edit profile form will go here]</h3>
-      <br />
-      <br />
-      <button type="submit" class="btn btn-primary">Save</button>
-      <button type="button" class="btn btn-default">Cancel</button>
-    </div>
-  `,
+  templateUrl: './profile.component.html'
 })
-export class ProfileComponent {
+export class ProfileComponent implements OnInit {
 
+  private profileFormGroup: FormGroup;
+
+  constructor(private authenticationService: AuthenticationService, private router: Router) {
+  }
+
+  ngOnInit(): void {
+    let firstName = new FormControl(this.authenticationService.currentUser.firstName);
+    let lastName = new FormControl(this.authenticationService.currentUser.lastName);
+    this.profileFormGroup = new FormGroup({
+      firstName: firstName,
+      lastName: lastName
+    });
+  }
+
+  cancel() {
+    this.router.navigate(['/events'])
+  }
+
+  saveProfile(value: any) {
+    this.authenticationService.updateCurrentUser(value.firstName, value.lastName);
+    this.router.navigate(['/events']);
+  }
 }
