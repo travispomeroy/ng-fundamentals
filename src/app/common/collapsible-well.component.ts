201a364898f156e8f1e8d5d8ee07e42116ba6127
Travis Pomeroy <travis.pomeroy84@gmail.com>
2019-04-19 22:38:28 -0400

    Added a fire icon to the session which have more than 3 votes as being a good session.

--- src/app/common/collapsible-well.component.ts
+++ src/app/common/collapsible-well.component.ts
@@ -1,23 +1,23 @@
-import {Component, Input} from "@angular/core";
+import {Component} from "@angular/core";
 
 @Component(
   {
     selector: 'collapsible-well',
-    template:`
+    template: `
       <div (click)="toggleContent()" class="well pointable">
-        <h4 class="well-title">{{title}}</h4>
-        <ng-content *ngIf="visible"></ng-content>
+        <h4>
+          <ng-content select="[well-title]"></ng-content>
+        </h4>
+        <ng-content *ngIf="visible" select="[well-body]"></ng-content>
       </div>
     `
   }
 )
 export class CollapsibleWellComponent {
 
-  @Input()
-  title: string;
-  visible: boolean = true;
+  private visible: boolean = true;
 
-  toggleContent() {
+  private toggleContent() {
     this.visible = !this.visible;
   }
 }
